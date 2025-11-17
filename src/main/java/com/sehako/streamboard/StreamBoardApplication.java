97aa09233f7a7e16aa53936aac9fe5c676781c16
sehako <dhtpgkr1999@gmail.com>
2025-11-17 20:37:17 +0900

    feat(init): 프로젝트 설정

---
+++ src/main/java/com/sehako/streamboard/StreamBoardApplication.java
@@ -0,0 +1,13 @@
+package com.sehako.streamboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sehako/streamboard/StreamBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sehako.streamboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
